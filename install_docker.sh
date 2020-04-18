5b1fae11a0598430fbc9ee5af622813a13c6b428
manee2k6 <manee2k6@gmail.com>
2020-04-18 12:29:14 +0530

    Create install_docker.sh

---
+++ install_docker.sh
@@ -0,0 +1,17 @@
+#Build a custom docker images with following software installed: sh, docker engine
+
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    bash \
+    software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce -y
