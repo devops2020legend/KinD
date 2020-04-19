d42a40719c02836260eb3d488b3262d41626bd07
manee2k6 <manee2k6@gmail.com>
2020-04-19 13:37:22 +0530

    Update install_kind.sh

--- install_kind.sh
+++ install_kind.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.7.0/kind-$(uname)-amd64 && chmod +x ./kind && mv ./kind /usr/local/bin
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.7.0/kind-$(uname)-amd64 && sudo chmod +x ./kind && sudo mv ./kind /usr/local/bin
 
 kind --version
